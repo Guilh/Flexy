374695d1cd6b1d09708a05ba72c7834e896db41e
Guil <guilhernandez@swanson.local>
2014-06-12 11:41:51 -0400

    First commit

---
+++ templates/project/manifest.rb
@@ -0,0 +1,19 @@
+# Description
+description "A simple Flexbox Compass extension."
+
+# Stylesheet Import
+file '_flexy-demo.scss', :like => :stylesheet, :media => 'screen, projection'
+
+# General File Import
+# file 'README.md', :to => "README.md"
+
+# Compass Extension Help
+help %Q{
+  Open flexy-demo.scss for help and examples.
+}
+
+# Compass Extension Welcome Message
+#  Users will see this when they create a new project using this template.
+welcome_message %Q{
+  You have successfully installed the Flexy demo! Open flexy-demo.scss for examples.
+}
